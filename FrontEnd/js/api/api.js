9c36faf4f18942a4679467d23d298086acaa1c0a
N0amG <noamguez0@gmail.com>
2025-02-19 16:20:49 +0100

    Add working filters

---
+++ FrontEnd/js/api/api.js
@@ -0,0 +1,15 @@
+const API_URL = "http://localhost:5678/api";
+
+export async function getData(endpoint) {
+    try {
+        console.log("Requete envoyé vers :", `${API_URL}/${endpoint}`);
+        const response = await fetch(`${API_URL}/${endpoint}`);
+        if (!response.ok) {
+            throw new Error(`Erreur: ${response.status} ${response.statusText}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+        return null;
+    }
+}
---
+++ FrontEnd/js/main.js
@@ -0,0 +1,4 @@
+
+if (document.URL.includes("index.html"))
+    //Afficher les projets si on se trouve sur la page d'accueil
+    import("./utils/projects.js");
